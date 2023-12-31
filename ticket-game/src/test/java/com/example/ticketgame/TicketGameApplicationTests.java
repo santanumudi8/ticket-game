ccc1da909388788026dde48878dd5cd89bdc9025
Santanu Mudi <santanumudi8@gmail.com>
2023-12-31 21:40:29 +0530

    initial commit

---
+++ ticket-game/src/test/java/com/example/ticketgame/TicketGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticketgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
